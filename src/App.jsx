e16e0919de01eaf23f389b66ed1e7bd4630f18c2
Jon Hogendorp <jonhoog@live.nl>
2023-10-03 16:44:15 +0200

    API local setup start working and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App(){
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+            const response = await axios.get('http://localhost:3001/books');
+            setBooks(response.data);
+    }
+
+    useEffect(() =>{
+        fetchBooks() 
+    }, []);
 
     const editBookById = (id, newTitle) => {
         const updatedBooks = books.map((book) => {
@@ -24,12 +34,12 @@ function App(){
         setBooks(updatedBooks);
     } ;
 
-    const createBook = (title) => {
-        console.log('Need to add book with; ', title)
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        })
         const updatedBooks = 
-        [...books,
-            {id:Math.round(Math.random() * 9999 ), 
-            title}];
+        [...books,response.data];
         setBooks(updatedBooks);
     };
 
